46e95b528a61247123c42a5d4ab3bdade47d5e7c
lijinrong <lijinrong@inke.com>
2021-12-30 17:43:36 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/entry.js',
@@ -18,5 +19,6 @@ export default {
       externalHelpers: true,
     }),
     commonjs(),
+    uglify(),
   ],
 };
